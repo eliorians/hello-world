4e9e3f74745de1d27db4c15c7219860070fb1c76
Eli Orians <oriansed@appstate.edu>
2022-09-06 10:31:04 -0400

    added some talk

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
        public static void main(String[] args) {
                  System.out.println("Hello, Professor.");
+                 java.util.Scanner keyboard = new java.util.Scanner(System.in);
+                 System.out.println("How are you?");
+                 String response = keyboard.nextLine();
                     }
 }
