05b96cd076ee75a8280e80fba88dd359eb487f16
Eli Orians <oriansed@appstate.edu>
2022-09-06 10:35:06 -0400

    partially done adding new question but exploring alt

--- Hello.java
+++ Hello.java
@@ -4,5 +4,7 @@ public class Hello {
                  java.util.Scanner keyboard = new java.util.Scanner(System.in);
                  System.out.println("How are you?");
                  String response = keyboard.nextLine();
+                 System.out.println("What did you do this weekend?");
+                 response = keyboard.nextLine();
                     }
 }
